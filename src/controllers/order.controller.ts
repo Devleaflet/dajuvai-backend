ae006874733ed84c68011595ed06f6a91f83f90d
Nirajan Thapa <thapanirajan789@gmail.com>
2025-09-02 13:03:46 +0545

    email update

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -62,17 +62,17 @@ export class OrderController {
             console.log("---------user email------------")
             console.log(useremail)
             // send customer email
-            await sendCustomerOrderEmail(
-                useremail,
-                order.id,
-                order.orderItems.map(item => ({
-                    name: item.product.name,
-                    sku: item.variant?.sku || null,
-                    quantity: item.quantity,
-                    price: item.price,
-                    variantAttributes: item.variant?.attributes || null
-                }))
-            )
+            // await sendCustomerOrderEmail(
+            //     useremail,
+            //     order.id,
+            //     order.orderItems.map(item => ({
+            //         name: item.product.name,
+            //         sku: item.variant?.sku || null,
+            //         quantity: item.quantity,
+            //         price: item.price,
+            //         variantAttributes: item.variant?.attributes || null
+            //     }))
+            // )
 
             const orderItems = order.orderItems
 
@@ -117,15 +117,15 @@ export class OrderController {
                 console.log(userexists.address.landmark);
 
 
-                await sendVendorOrderEmail(vendor.email, order.paymentMethod, order.id, itemsForVendor, {
-                    name: userexists.fullName,
-                    phone: userexists.phoneNumber,
-                    email: userexists.email,
-                    city: userexists.address.city,
-                    district: userexists.address.district,
-                    localAddress: userexists.address.localAddress,
-                    landmark: userexists.address.landmark
-                });
+                // await sendVendorOrderEmail(vendor.email, order.paymentMethod, order.id, itemsForVendor, {
+                //     name: userexists.fullName,
+                //     phone: userexists.phoneNumber,
+                //     email: userexists.email,
+                //     city: userexists.address.city,
+                //     district: userexists.address.district,
+                //     localAddress: userexists.address.localAddress,
+                //     landmark: userexists.address.landmark
+                // });
                 // name: string;
                 // phone: string;
                 // email ?: string;
@@ -155,7 +155,7 @@ export class OrderController {
                 });
                 res.status(error.status).json({
                     success: false,
-                    message: error.message, 
+                    message: error.message,
                 });
             }
 
@@ -173,10 +173,9 @@ export class OrderController {
                     ? error instanceof Error
                         ? error.message
                         : 'Unknown error'
-                    : 'Internal server error', 
+                    : 'Internal server error',
             });
         }
-
     }
 
     /**
